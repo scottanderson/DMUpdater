2717c36145e60df5ab43bdef245505a5fcff06b1
Scott Anderson <camel@droidmod.org>
2010-02-03 03:03:30 -0500

    bps smoothing

--- src/info/sholes/camel/updater/DownloadTask.java
+++ src/info/sholes/camel/updater/DownloadTask.java
@@ -14,25 +14,29 @@ public class DownloadTask extends AsyncTask<Object, Integer, Exception> {
 			OutputStream os = (OutputStream)params[1];
 
 			byte[] buf = new byte[40960];
-			long total = 0;
 			int read;
 			int readSinceProgress = 0;
-			long lastupdate = 0;
+			long lastupdate = System.currentTimeMillis();
 			long downloadStarted = System.currentTimeMillis();
+			float bps_avg = 0;
+			int avg_count = 0;
 
 			while((read = in.read(buf)) > 0) {
 				os.write(buf, 0, read);
 				readSinceProgress += read;
-				total += read;
 
 				if(readSinceProgress < 4096)
 					continue;
 
 				long now = System.currentTimeMillis();
 				if(now - lastupdate > 100) {
+					float bps = readSinceProgress * 1000 / (now - lastupdate);
+					bps_avg = ((bps_avg * avg_count) + bps) / (avg_count + 1);
+					if(avg_count < 200)
+						avg_count++;
+					publishProgress(new Integer(readSinceProgress), new Integer((int) bps_avg));
+
 					lastupdate = now;
-					int bps = (int) (total * 1000 / (now - downloadStarted));
-					publishProgress(new Integer(readSinceProgress), new Integer(bps));
 					readSinceProgress = 0;
 					Thread.yield();
 				}
